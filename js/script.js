10e1b2c12dee3b9ab491337dae1a43a62c7b0c6a
joshherkness <joshherkness@gmail.com>
2015-07-08 22:52:50 -0400

    Update
    Added some javascript

--- js/script.js
+++ js/script.js
@@ -1,17 +1,65 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+function startCounter(startDate) {
+    
+		setInterval(function() {
+        
+   	    // Gets the current date
+        var currentDate = new Date();
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 750, 'easeInOutExpo');
-        event.preventDefault();
-    });
-    $(".page-scroll a").removeClass('ui-btn-active');
+        // Calculates the time difference in ms
+        var timeDiff = currentDate - startDate;
+
+        // Convert to seconds
+        timeDiff /= 1000;
+
+        // Get seconds
+        var seconds = Math.round(timeDiff % 60);
+        // Remove seconds from the date
+        timeDiff = Math.floor(timeDiff / 60);
+
+        // Get minutes
+        var minutes = Math.round(timeDiff % 60);
+        // Remove minutes from the date
+        timeDiff = Math.floor(timeDiff / 60);
+
+        // Get hours
+        var hours = Math.round(timeDiff % 24);
+        // Remove hours from the date
+        timeDiff = Math.floor(timeDiff / 24);
+
+        // Get days
+        var days = Math.round(timeDiff % 365);
+        // Remove days from the date
+        timeDiff = Math.floor(timeDiff / 365);
+
+        // The rest of timeDiff is number of years
+        var years = timeDiff;
+                
+        $(".years").text(years);
+        $(".days").text(days);
+        $(".hours").text(hours);
+        $(".minutes").text(minutes);
+        $(".seconds").text(seconds);
+        
+    }, 1000);
+}
+
+window.onload = function () {
+    
+    var startDate = new Date("September 11, 2012 12:00:00");
+
+    setTimeout(startCounter(startDate), 1000);
+    
+};
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing');
+	});
 });
